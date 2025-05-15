2138aadaaa0c0accf5d7a2b936d6c451586278bb
HM745 <hmonpain09@gmail.com>
2025-05-16 00:05:36 +0200

    Újragondolás

--- 21-es_kartyajatek.py
+++ 21-es_kartyajatek.py
@@ -1,19 +1,52 @@
 import random
 print("Ez a 21-es kártyajáték")
-print("gep lapjai:")
-geplap1 = random.randint(2,11)
-print(geplap1)
-geplap2 = random.randint(2,11)
-print(geplap2)
-print("jatekos lapjai:")
-jatekos1_lap1 = random.randint(2,11)
-print(jatekos1_lap1)
-jatekos1_lap2 = random.randint(2,11)
-print(jatekos1_lap2)
+jatekmod = input("Gép vagy ember ellen szeretnél játszani?")
+def gep_verzio():
+    print("jétékos_lapjai:")
+    jatekos = []
+    jatekos.append(random.randint(2,11))
+    jatekos.append(random.randint(2,11))
+    print(jatekos)
+    jatekos_ertek = sum(jatekos)
+    print(f"Összesen:{jatekos_ertek}")
+    jatekos_db = len(jatekos)
+    print(f"Játékos lapjainak darab száma:{jatekos_db}")
+    print("")
+    print("gép lapjai:")
+    gep = []
+    gep.append(4)
+    gep.append(6)
+    print(gep)
+    gep_ertek = sum(gep)
+    print(f"Összesen:{gep_ertek}") 
+    gep_db = len(gep)
+    print(f"Gép lapjainak darab száma:{gep_db}")
 
-gep_osszesitett_ertek = geplap1 + geplap2
-print("Összesített érték:",gep_osszesitett_ertek)
+def ember_verzio():
+    print("játékos1 lapjai:")
+    jatekos1 = []
+    jatekos1.append(random.randint(2,11))
+    jatekos1.append(random.randint(2,11))
+    print(jatekos1)
+    jatekos1_ertek = sum(jatekos1)
+    print(f"Összesen:{jatekos1_ertek}")
+    jatekos1_db = len(jatekos1)
+    print(f"játékos1 lapjainak darab száma:{jatekos1_db}")
+    print("")
+    print("Játékos2 lapjai:")
+    jatekos2 = []
+    jatekos2.append(random.randint(2,11))
+    jatekos2.append(random.randint(2,11))
+    print(jatekos2)
+    jatekos2_ertek = sum(jatekos2)
+    print(f"Összesen:{jatekos2_ertek}")
+    jatekos2_db = len(jatekos2)
+    print(f"Játékos2 lapjainak darab száma:{ jatekos2_db}")
 
-jatekos1_osszesitett_ertek = jatekos1_lap1 + jatekos1_lap2
-print("Összesített érték:",jatekos1_osszesitett_ertek)
+    if jatekmod == "gép":
+        gep_verzio()
+    elif jatekmod == "ember":
+        ember_verzio()
+    else:
+        print("Érvénytelen játékmód.")
 
